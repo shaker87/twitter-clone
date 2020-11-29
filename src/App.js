bdd2eb47a0e41cfcbc3b6079844a67ae21479896
Shaker <sakerahmmed978@gmail.com>
2020-11-29 21:34:54 +0600

    add home and auth components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Login from "./Components/Auth/Login/Login";
 
 function App() {
   return (
     <>
-     <h1>shaker</h1>
+     <Router>
+       <Switch>
+         <Route path="/home">
+
+         </Route>
+         <Route path="/login">
+           <Login></Login>
+         </Route>
+       </Switch>
+     </Router>
     </>
   );
 }
